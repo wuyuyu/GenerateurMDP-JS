6243f8599def0f9c0d8ddfa462af5390f193b033
Wuyuyu <yuyuan.wu@hotmail.com>
2020-10-08 11:00:31 +0200

    add listener in slider and size

--- main.js
+++ main.js
@@ -2,6 +2,49 @@
 
 const p = new Password();
 
+const options = document.querySelector('#options');
+const size = document.querySelector('#size');
+const slider = document.querySelector('#slider');
+
+
+// addEventListener('type', callback(fonction de rapelle, ), boolean: direction de l'événement)
+size.addEventListener('input', event => {
+
+    console.log(event);
+    console.log(event.target === size); // élévement c'est size
+
+    slider.value = size.value;
+}); 
+
+slider.addEventListener('input', e => { size.value = slider.value });
+
+
+// pour récupérer l'endroit où utilisateur a cliqué sur la page
+const body = document.querySelector('body');
+body.addEventListener('click', evt => {
+    console.log(evt.clientX, evt.clientY);
+});
+
+
+
+rangeFormList();
+
+function rangeFormList() {
+    options.innerHTML='';
+    p.data.forEach(obj => {
+        options.innerHTML += `
+        <li class="list-group-item">
+        ${obj.name}
+        (${obj.chars})
+        <input type="checkbox" value="${obj.range}" ${ obj.range & p.range ? 'checked' : '' }/>
+        
+        </li>
+        `;
+    });
+   
+}
+
+
 // p.setRange(Password.all);
 // p.exclude(Password.num);
 // //p.include();
@@ -15,16 +58,3 @@ const p = new Password();
 // const mdp = document.getElementById('mdp');
 
 // mdp.innerText = p.generate(10);
-
-let options = document.querySelector('#options');
-
-p.data.forEach(obj => {
-    options.innerHTML += `
-        <li class="list-group-item">
-            ${obj.name}
-            (${obj.chars})
-        </li>
-    `;
-
-
-});
